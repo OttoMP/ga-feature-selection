794520a0b44db34c707acc805eaf2a321bf6c610
OttoMP <otto28mai@gmail.com>
2023-02-09 13:53:23 -0300

    major fixes

--- Feature-Selection/GA-on-Feature-Selection.py
+++ Feature-Selection/GA-on-Feature-Selection.py
@@ -1,212 +1,132 @@
 import numpy as np
 import pandas as pd
 #import time
-from sklearn import preprocessing
 import HParam_Opt_Functions as hp_opt
 
+def main():
+    # Loading the data, shuffling and preprocessing it
+    data = pd.read_csv("../Dataset/breast-cancer-wisconsin.csv")
+    data = data.sample(frac=1)
 
-# Loading the data, shuffling and preprocessing it
-data = pd.read_csv("../Dataset/breast-cancer-wisconsin.csv")
-data = data.sample(frac=1)
-
-# original data
-x_org_data = data.drop(["Y"],axis=1)
-y = pd.DataFrame(data,columns=["Y"]).values
-
-norm = preprocessing.MinMaxScaler()
-x = norm.fit_transform(x_org_data)
-
-data_count = len(x)
-print("number of observations:",data_count)
-print("-----------------------------------")
-
-
-# Hyperparameters (user inputted parameters)
-#-------------------------------------------
-prob_crsvr = 1 # probablity of crossover
-prob_mutation = 0.2 # probablity of mutation
-population = 80 # population number
-generations = 30 # generation number
-kfold = 5
-# chromosome encoding
-# one gene for each feature
-chromosome_size = 9
-
-# create an empty array to store a solution from each generation
-# for each generation, we want to save the best solution in that generation
-# to compare with the convergence of the algorithm
-best_of_a_generation = np.empty((0,chromosome_size+1))
-
-# so now, pool_of_solutions, has n (population) chromosomes
-# chromosome = np.array([0,1,0,0,0,1,0,0,1,0,0,1,
-#                        0,1,1,1,0,0,1,0,1,1,1,0]) # initial solution
-pool_of_solutions = np.array([np.random.randint(2, size=chromosome_size) for _ in range(population)])
-
-#start_time = time.time() # start time (timing purposes)
-
-for gen in range(generations): # do it n (generation) times
-
-    # an empty array for saving the new generation
-    # at the beginning of each generation, the array should be empty
-    # so that you put all the solutions created in a certain generation
-    new_population = np.empty((0,chromosome_size))
-
-    # an empty array for saving the new generation plus its obj func val
-    new_population_with_obj_val = np.empty((0,chromosome_size+1))
-
-    # an empty array for saving the best solution (chromosome)
-    # for each generation
-    sorted_best = np.empty((0,chromosome_size+1))
-
-    print("--> Generation: #", gen+1) # tracking purposes
+    n_samples = data.shape[0]
+    n_features = data.shape[1]-1
+    print("Number of samples:", n_samples)
+    print("Number of features:", n_features)
     print("-----------------------------------")
 
-    for family in range(int(population/2)): # population/2 because each gives 2 parents
 
-        print("--> Family: #", family+1) # tracking purposes
+    # Hyperparameters (user inputted parameters)
+    #-------------------------------------------
+    prob_crsvr = 1 # probablity of crossover
+    prob_mutation = 0.2 # probablity of mutation
+    population = 10 # population number
+    generations = 3 # generation number
+
+    # chromosome encoding
+    # one gene for each feature
+    chromosome_size = n_features
+
+    # create an empty array to store a solution from each generation
+    # for each generation, we want to save the best solution in that generation
+    # to compare with the convergence of the algorithm
+    best_of_a_generation = []
+
+    # so now, pool_of_solutions, has n (population) chromosomes
+    # chromosome = np.array([0,1,0,0,0,1,0,0,1,0,0,1,
+    #                        0,1,1,1,0,0,1,0,1,1,1,0]) # initial solution
+    pool_of_solutions = []
+    for _ in range(population):
+        parent = np.random.randint(2, size=chromosome_size)
+        fitness = hp_opt.objective_value(parent, data)
+        pool_of_solutions.append((fitness, parent))
+    pool_of_solutions = np.array(pool_of_solutions, dtype = object) # Convert for easier manipulation
+
+    # Start of Algorithm
+    # ------------------
+    for gen in range(generations):
+        print("--> Generation: #", gen+1)
         print("-----------------------------------")
 
-        # selecting 2 parents using tournament selection
-        # "genf.find_parents_ts"[0] gives parent_1
-        # "genf.find_parents_ts"[1] gives parent_2
-        parent_1 = hp_opt.find_parents_ts(pool_of_solutions,
-                                              x=x,y=y)
-        parent_2 = hp_opt.find_parents_ts(pool_of_solutions,
-                                              x=x,y=y)
+        # an empty array for saving the new generation
+        # at the beginning of each generation, the array should be empty
+        # so that you put all the solutions created in a certain generation
+        new_population = []
 
-        # crossover the 2 parents to get 2 children
-        # "genf.crossover"[0] gives child_1
-        # "genf.crossover"[1] gives child_2
-        child_1, child_2 = hp_opt.crossover(parent_1,parent_2,
-                                                prob_crsvr=prob_crsvr)
+        for family in range(int(population/2)): # population/2 because each gives 2 parents
+            print("--> Family: #", family+1)
+            print("-----------------------------------")
 
+            # selecting 2 parents using tournament selection
+            parent_1 = hp_opt.find_parents_ts(pool_of_solutions)
+            parent_2 = hp_opt.find_parents_ts(pool_of_solutions)
+
+            # crossover the 2 parents to get 2 children
+            child_1, child_2 = hp_opt.crossover(parent_1, parent_2
+                                                , prob_crsvr=prob_crsvr)
+
+        # mutating the 2 children to get 2 mutated children
         # mutating the 2 children to get 2 mutated children
         # "genf.mutation"[0] gives mutated_child_1
         # "genf.mutation"[1] gives mutated_child_2
-        hp_opt.mutation(child_1
+            # mutating the 2 children to get 2 mutated children
+        # "genf.mutation"[0] gives mutated_child_1
+        # "genf.mutation"[1] gives mutated_child_2
+            hp_opt.mutation(child_1
                             , prob_mutation=prob_mutation)
-        hp_opt.mutation(child_2
+            hp_opt.mutation(child_2
                             , prob_mutation=prob_mutation)
 
 
-        # getting the obj val (fitness value) for the 2 mutated children
-        # "genf.objective_value"[2] gives obj val for the mutated child
-        obj_val_mutated_child_1 = hp_opt.objective_value(x=x,y=y,
-                                                             chromosome=child_1,
-                                                             kfold=kfold)
-        obj_val_mutated_child_2 = hp_opt.objective_value(x=x,y=y,
-                                                             chromosome=child_2,
-                                                             kfold=kfold)
+            # getting the obj val (fitness value) for the 2 mutated children
+            obj_val_mutated_child_1 = hp_opt.objective_value(chromosome=child_1
+                                                             , data=data)
+            obj_val_mutated_child_2 = hp_opt.objective_value(chromosome=child_2
+                                                             , data=data)
 
 
-        # for each mutated child, put its obj val next to it
-        mutant_1_with_obj_val = np.hstack((obj_val_mutated_child_1,
-                                               child_1)) # lines 132 and 140
+            # for each mutated child, put its obj val next to it
+            mutant_1_with_obj_val = (obj_val_mutated_child_1, child_1)
 
-        mutant_2_with_obj_val = np.hstack((obj_val_mutated_child_2,
-                                               child_2)) # lines 134 and 143
+            mutant_2_with_obj_val = (obj_val_mutated_child_2, child_2)
 
 
-        # we need to create the new population for the next generation
-        # so for each family, we get 2 solutions
-        # we keep on adding them till we are done with all the families in one generation
-        # by the end of each generation, we should have the same number as the initial population
-        # so this keeps on growing and growing
-        # when it's a new generation, this array empties and we start the stacking process
-        # and so on
-        # check line 88
-        new_population = np.vstack((new_population,
-                                    child_1,
-                                    child_2))
+            new_population.append(mutant_1_with_obj_val)
+            new_population.append(mutant_2_with_obj_val)
 
 
-        # same explanation as above, but we include the obj val for each solution as well
-        # check line 91
-        new_population_with_obj_val = np.vstack((new_population_with_obj_val,
-                                                 mutant_1_with_obj_val,
-                                                 mutant_2_with_obj_val))
-        # after getting 2 mutated children (solutions), we get another 2, and so on
-        # until we have the same number of the intended population
-        # then we go to the next generation and start over
-        # since we ended up with 2 solutions, we move on to the next possible solutions
+        # we replace the initial (before) population with the new one (current generation)
+        # this new pool of solutions becomes the starting population of the next generation
+        pool_of_solutions = np.array(new_population, dtype=object)
+        best_of_a_generation.append(sorted(new_population,key=lambda x: x[0])[0])
 
-
-    # we replace the initial (before) population with the new one (current generation)
-    # this new pool of solutions becomes the starting population of the next generation
-    pool_of_solutions = new_population
-
-
-    # for each generation
-    # we want to find the best solution in that generation
-    # so we sort them based on index [0], which is the obj val
-    sorted_best = np.array(sorted(new_population_with_obj_val,
-                                               key=lambda x:x[0]))
-
+    best_last_generation = best_of_a_generation[-1]
+    best_of_all = sorted(best_of_a_generation, key=lambda x:x[0])[0]
 
     # since we sorted them from best to worst
-    # the best in that generation would be the first solution in the array
-    # so index [0] of the "sorted_best" array
-    best_of_a_generation = np.vstack((best_of_a_generation,
-                                      sorted_best[0]))
+    # the best would be the first solution in the array
+    # so index [0] of the "sorted_last_population" array
+    best_string_convergence = best_last_generation[1]
+    emp_list_convergence = [i for i,e in enumerate(best_string_convergence) if e == 1]
 
+    best_string_overall = best_of_all[1]
+    emp_list_overall = [i for i,e in enumerate(best_string_overall) if e == 1]
 
-#end_time = time.time() # end time (timing purposes)
+    print("\n------------------------------\n")
+    print("Final Solution (Convergence):",best_last_generation[1]) # final solution entire chromosome
+    print("Obj Value - Convergence:",round(best_last_generation[0]),5) # obj val of final chromosome
+    print("Features included are:",emp_list_convergence)
+    print()
+    print("Final Solution (Best):",best_of_all[1]) # final solution entire chromosome
+    print("Obj Value - Best in Generations:",round(best_of_all[0]),5) # obj val of final chromosome
+    print("Features included are:",emp_list_overall)
 
+    # to decode the x and y chromosomes to their real values
+    #final_solution_convergence = hp_opt.objective_value(chromosome=best_string_convergence
+                                                        #, data=data)
 
-# for our very last generation, we have the last population
-# for this array of last population (convergence), there is a best solution
-# so we sort them from best to worst
-sorted_last_population = np.array(sorted(new_population_with_obj_val,
-                                         key=lambda x:x[0]))
+    #final_solution_overall = hp_opt.objective_value(chromosome=best_string_overall
+                                                    #, data=data)
+    print("------------------------------")
 
-sorted_best_of_a_generation = np.array(sorted(best_of_a_generation,
-                                         key=lambda x:x[0]))
-
-sorted_last_population[:,0] = 1-(sorted_last_population[:,0]) # get accuracy instead of error
-sorted_best_of_a_generation[:,0] = 1-(sorted_best_of_a_generation[:,0])
-
-# since we sorted them from best to worst
-# the best would be the first solution in the array
-# so index [0] of the "sorted_last_population" array
-best_string_convergence = sorted_last_population[0]
-emp_list_convergence = [i for i,e in enumerate(best_string_convergence) if e == 1]
-
-best_string_overall = sorted_best_of_a_generation[0]
-emp_list_overall = [i for i,e in enumerate(best_string_overall) if e == 1]
-
-print()
-#print()
-#print("Execution Time in Minutes:",(end_time - start_time)/60) # exec. time
-
-
-print()
-print()
-print("------------------------------")
-print()
-#print("Execution Time in Seconds:",end_time - start_time) # exec. time
-#print()
-print("Final Solution (Convergence):",best_string_convergence[1:]) # final solution entire chromosome
-print()
-print("Final Solution (Best):",best_string_overall[1:]) # final solution entire chromosome
-
-# to decode the x and y chromosomes to their real values
-final_solution_convergence = hp_opt.objective_value(x=x,y=y,
-                                                        chromosome=best_string_convergence[1:],
-                                                        kfold=kfold)
-
-final_solution_overall = hp_opt.objective_value(x=x,y=y,
-                                                    chromosome=best_string_overall[1:],
-                                                    kfold=kfold)
-
-# the "svm_hp_opt.objective_value" function returns 3 things -->
-# [0] is the x value
-# [1] is the y value
-# [2] is the obj val for the chromosome (avg. error)
-print()
-print("Obj Value - Convergence:",round(1-(final_solution_convergence),5)) # obj val of final chromosome
-print("Features included are:",emp_list_convergence)
-print()
-print("Obj Value - Best in Generations:",round(1-(final_solution_overall),5)) # obj val of final chromosome
-print("Features included are:",emp_list_overall)
-print()
-print("------------------------------")
+if __name__ == "__main__":
+    main()
--- Feature-Selection/HParam_Opt_Functions.py
+++ Feature-Selection/HParam_Opt_Functions.py
@@ -1,14 +1,22 @@
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import KFold
+from sklearn import preprocessing
 from sklearn import svm
 from copy import deepcopy
 
 
 
-# calculate fitness value for the chromosome of 0s and 1s
-def objective_value(x,y,chromosome,kfold=3):
-    emp_list = [i for i,e in enumerate(chromosome) if e == 1]
+def objective_value(chromosome, data):
+    # original data
+    x_org_data = data.drop(["Y"],axis=1)
+    y = pd.DataFrame(data,columns=["Y"]).values
 
+    norm = preprocessing.MinMaxScaler()
+    x = norm.fit_transform(x_org_data)
+
+    # Reading Chromosome
+    emp_list = [i for i,e in enumerate(chromosome) if e == 1]
     # To cover the case when chromoseme = [0, ... , 0]
     # and prevent program to crash
     if emp_list == []:
@@ -16,6 +24,7 @@ def objective_value(x,y,chromosome,kfold=3):
 
     new_x = x[:,emp_list]
 
+    kfold = 3
     kf = KFold(n_splits=kfold)
     # objective function value for the decoded x and decoded y
     sum_of_error = 0
@@ -34,103 +43,46 @@ def objective_value(x,y,chromosome,kfold=3):
     return avg_error
 
 
-# finding 2 parents from the pool of solutions
-# using the tournament selection method
-def find_parents_ts(all_solutions,x,y):
-
-    # select 3 random parents from the pool of solutions you have
-    posb_parents = all_solutions[np.random.choice(all_solutions.shape[0], 3, replace=False)]
-
-    # get objective function value (fitness) for each possible parent
-    # index no.2 because the objective_value function gives the fitness value at index no.2
-    tournament = sorted([(parent, objective_value(x,y,parent)) for parent in posb_parents], key = lambda x: x[1])
-
-    return tournament[0][0]
+def find_parents_ts(all_solutions):
+    # Tournament Selection
+    tournament = sorted(all_solutions[np.random.choice(all_solutions.shape[0], 3, replace=False)]
+                        , key = lambda x: x[0])
+    return tournament[0][1]
 
 
-# crossover between the 2 parents to create 2 children
-# functions inputs are parent_1, parent_2, and the probability you would like for crossover
-# default probability of crossover is 1
 def crossover(parent_1,parent_2,prob_crsvr=1):
-
-    rand_num_to_crsvr_or_not = np.random.rand() # do we crossover or not???
-    if rand_num_to_crsvr_or_not < prob_crsvr:
+    crsvr_or_not = np.random.rand()
+    if crsvr_or_not < prob_crsvr:
         index_1 = np.random.randint(0,len(parent_1))
         index_2 = np.random.randint(index_1,len(parent_1))
 
-        ### FOR PARENT_1 ###
-
-        # first_seg_parent_1 -->
-        # for parent_1: the genes from the beginning of parent_1 to the
-                # beginning of the middle segment of parent_1
         first_seg_parent_1 = parent_1[:index_1]
-
-        # middle segment; where the crossover will happen
-        # for parent_1: the genes from the index chosen for parent_1 to
-                # the index chosen for parent_2
         mid_seg_parent_1 = parent_1[index_1:index_2+1]
-
-        # last_seg_parent_1 -->
-        # for parent_1: the genes from the end of the middle segment of
-                # parent_1 to the last gene of parent_1
         last_seg_parent_1 = parent_1[index_2+1:]
 
-
-        ### FOR PARENT_2 ###
-
-        # first_seg_parent_2 --> same as parent_1
         first_seg_parent_2 = parent_2[:index_1]
-
-        # mid_seg_parent_2 --> same as parent_1
         mid_seg_parent_2 = parent_2[index_1:index_2+1]
-
-        # last_seg_parent_2 --> same as parent_1
         last_seg_parent_2 = parent_2[index_2+1:]
 
 
-        ### CREATING CHILD_1 ###
-
-        # the first segment from parent_1
-        # plus the middle segment from parent_2
-        # plus the last segment from parent_1
+        ### CHILD_1 ###
+        # parent_1 | parent_2 | parent_1
         child_1 = np.concatenate((first_seg_parent_1,mid_seg_parent_2,
                                   last_seg_parent_1))
-
-
-        ### CREATING CHILD_2 ###
-
-        # the first segmant from parent_2
-        # plus the middle segment from parent_1
-        # plus the last segment from parent_2
+        ### CHILD_2 ###
+        # parent_2 | parent_1 | parent_2
         child_2 = np.concatenate((first_seg_parent_2,mid_seg_parent_1,
                                   last_seg_parent_2))
 
-
-    # when we will not crossover
-    # when rand_num_to_crsvr_or_not is NOT less (is greater) than prob_crsvr
-    # when prob_crsvr == 1, then rand_num_to_crsvr_or_not will always be less
-            # than prob_crsvr, so we will always crossover then
     else:
         child_1 = deepcopy(parent_1)
         child_2 = deepcopy(parent_2)
 
-    return child_1,child_2 # the defined function will return 2 arrays
+    return child_1,child_2
 
 
-
-############################################################
-### MUTATING THE TWO CHILDREN TO CREATE MUTATED CHILDREN ###
-############################################################
-
-# mutation for the 2 children
-# functions inputs are child_1, child_2, and the probability you would like for mutation
-# default probability of mutation is 0.2
-def mutation(child_1,prob_mutation=0.2):
-
-    for index in range(len(child_1)): # for each gene (index)
-
-        rand_num_to_mutate_or_not = np.random.rand() # do we mutate or no???
-        # if the rand_num_to_mutate_or_not is less that the probability of mutation
-                # then we mutate at that given gene (index we are currently at)
-        if rand_num_to_mutate_or_not < prob_mutation:
-            child_1[index] = 1 - child_1[index] #flip gene
+def mutation(child,prob_mutation=0.2):
+    for index in range(len(child)):
+        mutate_or_not = np.random.rand()
+        if mutate_or_not < prob_mutation:
+            child[index] = 1 - child[index] #flip gene
